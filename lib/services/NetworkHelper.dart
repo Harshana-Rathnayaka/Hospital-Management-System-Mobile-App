8f2a7c45b38d9083c94f684bbeed8cc7976e5550
Harshana ratnayaka <huratnayaka@gmail.com>
2021-01-14 22:40:24 +0530

    backend connection done

---
+++ lib/services/NetworkHelper.dart
@@ -0,0 +1,30 @@
+import 'package:connectivity/connectivity.dart';
+import 'package:http/http.dart' as http;
+
+class Network {
+  // for real device
+  final String url = "http://0.0.0.0:8000/api";
+
+  postData(values, endpoint) async {
+    var fullUrl = url + endpoint;
+
+    return await http.post(fullUrl, body: values);
+  }
+
+  getData(endpoint) async {
+    var fullUrl = url + endpoint;
+
+    return await http.get(fullUrl);
+  }
+
+  checkInternetConnection() async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+
+    if (connectivityResult == ConnectivityResult.none) {
+      return false;
+    } else if (connectivityResult == ConnectivityResult.wifi ||
+        connectivityResult == ConnectivityResult.mobile) {
+      return true;
+    }
+  }
+}
