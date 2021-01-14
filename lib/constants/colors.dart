a8900eefde50030b41029db3b39bd172f66a1035
Harshana ratnayaka <huratnayaka@gmail.com>
2021-01-14 22:39:54 +0530

    card color changed

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -4,6 +4,6 @@ const primaryColor = Color(0xFF673AB7);
 const backgroundColor = Color(0xFFF0F0F0);
 const appBarColor = Color(0xFFDBD9D9);
 const colorGrey = Color(0xFFADACAC);
-const cardColor = Color(0xFFE0F2F1);
+const cardColor = Color(0xFFF3E5F5);
 const hintColor = Color(0xFF455A64);
 const errorColor = Color(0xFFF44336);
