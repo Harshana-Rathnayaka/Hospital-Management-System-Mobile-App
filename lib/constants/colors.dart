3380e4f74011c0709bdae16c8da0f0ce5a5f4598
Harshana ratnayaka <huratnayaka@gmail.com>
2021-01-14 17:01:55 +0530

    images and colours added

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const primaryColor = Color(0xFF673AB7);
+const backgroundColor = Color(0xFFF0F0F0);
+const appBarColor = Color(0xFFDBD9D9);
+const colorGrey = Color(0xFFADACAC);
+const cardColor = Color(0xFFE0F2F1);
+const hintColor = Color(0xFF455A64);
+const errorColor = Color(0xFFF44336);
---
+++ lib/constants/images.dart
@@ -0,0 +1,2 @@
+const login_image = "assets/images/login_image.svg";
+const signup_image = "assets/images/signup_image.svg";
